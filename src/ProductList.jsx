ef3d2d98e43345eb6a1bf8b0cb68b3f6316d1056
AditSecureDev <aditsharma171@gmail.com>
2026-02-21 05:29:47 -0500

    Implemented ProductList layout with add to cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -232,7 +232,13 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
-
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
+    };
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -274,6 +280,35 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+
+                    {plantsArray.map((category, index) => (
+                        <div key={index}> { }
+                            <h1>
+                                <div>{category.category}</div> { }
+                            </h1>
+                            <div className="product-list"> { }
+                                {category.plants.map((plant, plantIndex) => (
+                                    <div className="product-card" key={plantIndex}> { }
+                                        <img
+                                            className="product-image"
+                                            src={plant.image}
+                                            alt={plant.name}
+                                        />
+                                        <div className="product-title">{plant.name}</div> { }
+                                        { }
+                                        <div className="product-description">{plant.description}</div> { }
+                                        <div className="product-cost">${plant.cost}</div> { }
+                                        <button
+                                            className="product-button"
+                                            onClick={() => handleAddToCart(plant)}
+                                        >
+                                            Add to Cart
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
 
 
                 </div>
