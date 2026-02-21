579577d823d6081ee20f02c91107a180b1dc0765
AditSecureDev <aditsharma171@gmail.com>
2026-02-21 06:28:04 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To Our Paradise Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
+            Shop now
           </button>
          </div>
           <div className="aboutus_container">
